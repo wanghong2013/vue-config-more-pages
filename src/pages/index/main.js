abf5d593ee294f99a707336dc3bd1a9a65b5623b
wanghong <34284143@qq.com>
2020-10-17 11:36:12 +0800

    x

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import {Spin} from "ant-design-vue"
-Vue.use(Spin)
-window.spin = Spin
 Vue.config.productionTip = false
 new Vue({
   router,
